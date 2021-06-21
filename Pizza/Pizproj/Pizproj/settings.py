d63c9cbc2f4b4284d2b22890b896866eadd8c35d
Omkar <omkarhalgi50@gmail.com>
2021-06-21 17:44:26 +0530

    host-allowed

--- Pizza/Pizproj/Pizproj/settings.py
+++ Pizza/Pizproj/Pizproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uv5&xvgnhe-ay2x8^^fg+6hdsdn*$-uu)!86==(%645z^sf4wp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['localhost', '127.0.0.1']
 
 
 # Application definition
