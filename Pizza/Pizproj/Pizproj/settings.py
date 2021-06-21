940b9ddf30761c54e4ebe894da7f6386d982c433
Omkar <omkarhalgi50@gmail.com>
2021-06-21 17:37:29 +0530

    debug

--- Pizza/Pizproj/Pizproj/settings.py
+++ Pizza/Pizproj/Pizproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uv5&xvgnhe-ay2x8^^fg+6hdsdn*$-uu)!86==(%645z^sf4wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
